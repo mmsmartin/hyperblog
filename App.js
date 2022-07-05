0e3da27a3cc43ea6191dbfb6db95652fa382ed6e
mmsmartin <mmsmartin@gmail.com>
2022-07-05 15:12:57 -0500

    I added new last name function

--- App.js
+++ App.js
@@ -2,7 +2,13 @@ function setName(p_name){
     console.log("This is the name:" + p_name);
 }
 
+function setLastName(p_lastName){
+    console.log("This is the last name:" + p_lastName);
+}
+
+
 function main(){
     setName("Martín");
+    setLastName("Muñoz Sánchez");
 }
 
